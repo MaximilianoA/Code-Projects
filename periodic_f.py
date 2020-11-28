b3728e61b29996044146f96b69724e48d6cf46ea
Max Ayala <maxcayala@gmail.com>
2020-11-28 06:01:14 -0800

    Implement periodic function

--- periodic_f.py
+++ periodic_f.py
@@ -9,3 +9,28 @@
 import numpy as np
 
 f = lambda z,c: z**2 + c
+ITER_MAX = 30
+
+# method to find number of iterations i
+# if i reaches ITTER_MAX assume non-periodic
+# @return: weather or not these values are periodic
+# @param: z and c MUST be of type float
+def test_if_periodic(z, c, view=False):
+    z0 = z
+    periodic = False
+    for i in range(1, ITER_MAX):
+        z = f(z, c)
+        if view:
+            print(z)
+        if z==z0:
+            periodic = True
+            break
+    if periodic:
+        print(
+            'Z =', z0,
+            'c =', c,
+            'i =', i, 
+            )
+    else:
+        print('non-periodic')
+    return periodic
